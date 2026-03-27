c2a1a8f057f357791f0605b25424760de6c69d71
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-27 15:34:32 +0900

    first commit

---
+++ src/test/java/com/b1uffer/mysecurity/MySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1uffer.mysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

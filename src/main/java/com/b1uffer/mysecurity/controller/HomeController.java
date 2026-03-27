db39fe04dfae49574677a41d8372724082b01bbf
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-27 16:08:37 +0900

    HomeController 구현

--- src/main/java/com/b1uffer/mysecurity/controller/HomeController.java
+++ src/main/java/com/b1uffer/mysecurity/controller/HomeController.java
@@ -1,4 +1,22 @@
 package com.b1uffer.mysecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "home";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin/panel")
+    @ResponseBody
+    public String admin() {
+        return "admin";
+    }
 }

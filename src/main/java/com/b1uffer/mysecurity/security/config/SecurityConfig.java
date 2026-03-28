b8a000e6c958c9aab199a9d8dc2b69b7756431a4
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-28 15:18:23 +0900

    서블릿(Spring MVC) 기반 FilterChain 주석처리

--- src/main/java/com/b1uffer/mysecurity/security/config/SecurityConfig.java
+++ src/main/java/com/b1uffer/mysecurity/security/config/SecurityConfig.java
@@ -1,50 +1,43 @@
 package com.b1uffer.mysecurity.security.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/public/**").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated())
-                .formLogin(login -> login
-                        /**
-                         * loginPage 메서드를 집어넣음으로 login 컨트롤러와 정적페이지를 만들어줘야함
-                         */
-                        .loginPage("/login").permitAll()
-                        .defaultSuccessUrl("/", true))
-                .logout(logout -> logout
-                        .logoutUrl("/logout"));
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/", "/public/**").permitAll()
+//                        .requestMatchers("/admin").hasRole("ADMIN")
+//                        .anyRequest().authenticated())
+//                .formLogin(login -> login
+//                        /**
+//                         * loginPage 메서드를 집어넣음으로 login 컨트롤러와 정적페이지를 만들어줘야함
+//                         */
+//                        .loginPage("/login").permitAll()
+//                        .defaultSuccessUrl("/", true))
+//                .logout(logout -> logout
+//                        .logoutUrl("/logout"));
+//
+//        return http.build();
+//    }
 
-        return http.build();
-    }
-
-    @Bean
-    UserDetailsService users() {
-        UserDetails user = User.withUsername("user")
-                .password("{noop}pass")
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.withUsername("admin")
-                .password("{noop}pass")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+//    @Bean
+//    UserDetailsService users() {
+//        UserDetails user = User.withUsername("user")
+//                .password("{noop}pass")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.withUsername("admin")
+//                .password("{noop}pass")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 }

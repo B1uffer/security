6a60489fb879ccc4969947d95b5c0a32b62fb96d
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-28 16:38:43 +0900

    WebFlux 반환타입에 맞게 리턴값 수정 테스트 메서드 추가

--- src/main/java/com/b1uffer/mysecurity/controller/LoginController.java
+++ src/main/java/com/b1uffer/mysecurity/controller/LoginController.java
@@ -1,12 +1,24 @@
 package com.b1uffer.mysecurity.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public Mono<String> login() {
+        return Mono.just("login");
+    }
+
+    @GetMapping("/me")
+    @ResponseBody
+    public Mono<String> me(Authentication authentication) {
+        if (authentication == null) {
+            return Mono.just("anonymous");
+        }
+        return Mono.just(authentication.getName() + " / " + authentication.getAuthorities());
     }
 }

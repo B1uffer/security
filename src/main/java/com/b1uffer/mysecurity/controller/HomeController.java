da8fcb4b83cf36ced65083ac02fc19536e94a509
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-28 16:39:16 +0900

    API 리턴타입을 WebFlux에 맞게 수정 PreAuthorize에 대한 주석 추가

--- src/main/java/com/b1uffer/mysecurity/controller/HomeController.java
+++ src/main/java/com/b1uffer/mysecurity/controller/HomeController.java
@@ -4,19 +4,25 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     @ResponseBody
-    public String home() {
-        return "home";
+    public Mono<String> home() {
+        return Mono.just("home");
     }
 
+    /**
+     * 리액티브(WebFlux) 기반 FilterChain을 사용할 때
+     * FilterChain에 formLogin()과 httpBasic()을 둘 다 사용할때
+     * 403이 뜰 수 있다
+     */
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin/panel")
     @ResponseBody
-    public String admin() {
-        return "admin";
+    public Mono<String> admin() {
+        return Mono.just("admin");
     }
 }

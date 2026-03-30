499ec5edb55c1328494ea0d168f264a2e04764e6
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-30 14:43:11 +0900

    주석 추가

--- src/main/java/com/b1uffer/mysecurity/filter/config/FilterConfiguration.java
+++ src/main/java/com/b1uffer/mysecurity/filter/config/FilterConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfiguration {
+    // 서블릿 기반 Filter라서 리액티브 기반(WebFlux)에서는 필터가 동작하지 않는다
     @Bean
     public FilterRegistrationBean<FirstFilter> firstFilterRegistration() {
         FilterRegistrationBean<FirstFilter> firstFilterRegistration = new FilterRegistrationBean<>(new FirstFilter());

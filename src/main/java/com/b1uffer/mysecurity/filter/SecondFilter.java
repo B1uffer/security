61964614b2bf8c997173fb3caf0422979850d3c9
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-30 15:09:12 +0900

    SecondFilter 클래스 생성 및 인터페이스 구현

---
+++ src/main/java/com/b1uffer/mysecurity/filter/SecondFilter.java
@@ -0,0 +1,33 @@
+package com.b1uffer.mysecurity.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+
+public class SecondFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        System.out.println("---- SecondFilter init ----");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("---- SecondFilter start ----");
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("---- SecondFilter end ----");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("---- SecondFilter destroy ----");
+        Filter.super.destroy();
+    }
+}

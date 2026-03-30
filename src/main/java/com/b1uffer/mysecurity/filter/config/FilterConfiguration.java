d5ae61dd6f76f5d078b3e90c84c2a879a2fe07e7
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-30 14:35:03 +0900

    firstFilterRegistration 메서드 생성

--- src/main/java/com/b1uffer/mysecurity/filter/config/FilterConfiguration.java
+++ src/main/java/com/b1uffer/mysecurity/filter/config/FilterConfiguration.java
@@ -1,8 +1,14 @@
 package com.b1uffer.mysecurity.filter.config;
 
+import com.b1uffer.mysecurity.filter.FirstFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfiguration {
+    @Bean
+    public FilterRegistrationBean<FirstFilter> firstFilterRegistration() {
 
+    }
 }

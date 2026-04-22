d8e55744678e19a7562fa6cf034a2166fa4e3391
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-22 14:23:51 +0900

    필터 아키텍처, 필터 순서와 우선순위 FirstFilter 클래스 구현

---
+++ src/main/java/com/b1uffer/mysecurity/security/Filter/FirstFilter.java
@@ -0,0 +1,25 @@
+package com.b1uffer.mysecurity.security.Filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import org.springframework.core.annotation.Order;
+
+import java.io.IOException;
+
+/**
+ * @Order는 해당 필터를 우선적으로 실행하게끔 한다
+ * 괄호안의 숫자가 작을수록 우선순위가 높다, 즉 먼저 실행된다
+ */
+@Order(1)
+public class FirstFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("First Filter 실행");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}

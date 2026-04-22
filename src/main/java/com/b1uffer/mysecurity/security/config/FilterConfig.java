5f3c359c187ef0e7b543612dce87eb2c65950192
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-22 14:50:02 +0900

    필터 아키텍처, 필터 순서와 우선순위 FilterConfig 구현

---
+++ src/main/java/com/b1uffer/mysecurity/security/config/FilterConfig.java
@@ -0,0 +1,29 @@
+package com.b1uffer.mysecurity.security.config;
+
+import com.b1uffer.mysecurity.security.Filter.FirstFilter;
+import com.b1uffer.mysecurity.security.Filter.SecondFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<FirstFilter> firstFilterRegistration() {
+        FilterRegistrationBean<FirstFilter> registration =
+                new FilterRegistrationBean<>(new FirstFilter());
+
+        registration.setOrder(1); // 먼저 실행하기
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean<SecondFilter> secondFilterRegistration() {
+        FilterRegistrationBean<SecondFilter> registration =
+                new FilterRegistrationBean<>(new SecondFilter());
+
+        registration.setOrder(2); // 두번째로 실행하기
+        return registration;
+    }
+}

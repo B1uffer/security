6e9a40c8e7fdd45a95296b60fb96b4cb727f2343
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-24 16:55:33 +0900

    AuthenticationEventListener 구현

--- src/main/java/com/b1uffer/mysecurity/security/event/AuthenticationEventListener.java
+++ src/main/java/com/b1uffer/mysecurity/security/event/AuthenticationEventListener.java
@@ -1,4 +1,24 @@
 package com.b1uffer.mysecurity.security.event;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AuthenticationEventListener {
+    @EventListener
+    public void handleSuccess(AuthenticationSuccessEvent event) {
+        System.out.println("로그인 성공 : " + event.getAuthentication().getName());
+    }
+
+    /**
+     * AuthenticationFailureEvent는 AbstractAuthenticationFailureEvent 클래스 형태로
+     * 다른 AuthenticationFailureEvent들에 상속되어 사용할 수 있다
+     * FailureEvent를 여러개로 쪼개놓았음, 구체적으로 사용할 수 있다
+     */
+    @EventListener
+    public void handleFailure(AuthenticationFailureBadCredentialsEvent event) {
+        System.out.println("로그인 실패" + event.getException().getMessage());
+    }
 }
